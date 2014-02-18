880e2f0696d20c23ef695ebbcf295ad97efd9f92
Matt Hayes <mhayes@uship.com>
2014-02-18 11:49:40 -0600

    Initial Commit

---
+++ OCMockSingletonsExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OCMockSingletonsExample
+//
+//  Created by Matt Hayes on 2/18/14.
+//  Copyright (c) 2014 uShip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OCMockSingletonsExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OCMockSingletonsExample
+//
+//  Created by Matt Hayes on 2/18/14.
+//  Copyright (c) 2014 uShip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
